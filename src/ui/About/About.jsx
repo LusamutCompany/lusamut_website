dad001b59ef720bb60c874986b8aeb2457241436
LusamutCompany <lusamutcompany@gmail.com>
2023-12-18 04:09:13 +0400

    added slider

--- src/ui/About/About.jsx
+++ src/ui/About/About.jsx
@@ -1,26 +1,94 @@
+import { useState, useRef, useEffect } from "react"
 import aboutStyles from "./About.module.css"
 import aboutImage from "./../../assets/pngs/Image.png"
 
+const pages = [
+  {
+    title: "ՄԵՐ ՄԱՍԻՆ",
+    text: " Հայտնի է, որ ընթերցողը, կարդալով հասկանալի տեքստ, չի կարողանա կենտրոնանալ տեքստի ձևավորման վրա: Lorem Ipsum օգտագործելը բացատրվում է նրանով, որ այն բաշխում է բառերը քիչթե շատ իրականի նման, ի տարբերություն «Բովանդակություն, բովանդակություն» սովորական կրկննության, ինչը ընթերցողի համար հասկանալի է:",
+    text2: " Շատ համակարգչային տպագրական ծրագրեր և ինտերնետային էջերի խմբագրիչներ այն օգտագործում են որպես իրենց ստանդարտ տեքստային մոդել, և հետևապես, ինտերնետում Lorem Ipsum-ի որոնման արդյունքում կարելի է հայտնաբերել էջեր, որոնք դեռ նոր են կերտվում:",
+    image: aboutImage
+  },
+  {
+    title: "ՄԵՐ ՄԱՍԻՆ2",
+    text: " Հայտնի է, որ ընթերցողը, կարդալով հասկանալի տեքստ, չի կարողանա կենտրոնանալ տեքստի ձևավորման վրա: Lorem Ipsum օգտագործելը բացատրվում է նրանով, որ այն բաշխում է բառերը քիչթե շատ իրականի նման, ի տարբերություն «Բովանդակություն, բովանդակություն» սովորական կրկննության, ինչը ընթերցողի համար հասկանալի է:",
+    text2: " Շատ համակարգչային տպագրական ծրագրեր և ինտերնետային էջերի խմբագրիչներ այն օգտագործում են որպես իրենց ստանդարտ տեքստային մոդել, և հետևապես, ինտերնետում Lorem Ipsum-ի որոնման արդյունքում կարելի է հայտնաբերել էջեր, որոնք դեռ նոր են կերտվում:",
+    image: aboutImage
+  },
+  {
+    title: "ՄԵՐ ՄԱՍԻՆ3",
+    text: " Հայտնի է, որ ընթերցողը, կարդալով հասկանալի տեքստ, չի կարողանա կենտրոնանալ տեքստի ձևավորման վրա: Lorem Ipsum օգտագործելը բացատրվում է նրանով, որ այն բաշխում է բառերը քիչթե շատ իրականի նման, ի տարբերություն «Բովանդակություն, բովանդակություն» սովորական կրկննության, ինչը ընթերցողի համար հասկանալի է:",
+    text2: " Շատ համակարգչային տպագրական ծրագրեր և ինտերնետային էջերի խմբագրիչներ այն օգտագործում են որպես իրենց ստանդարտ տեքստային մոդել, և հետևապես, ինտերնետում Lorem Ipsum-ի որոնման արդյունքում կարելի է հայտնաբերել էջեր, որոնք դեռ նոր են կերտվում:",
+    image: aboutImage
+  }
+]
+let isSliding = true
 function About() {
+  const aboutRef = useRef(null)
+  const [index, setIndex] = useState(0)
+  useEffect(() => {
+    if (isSliding) {
+      setTimeout(() => {
+        if(isSliding){
+          moveRightLogic()
+        }
+      }, 3000)
+    }
+  }, [index])
+  function moveRightLogic() {
+    if (index < pages.length - 1) {
+      setIndex(index + 1)
+      aboutRef.current.scrollBy({ left: window.innerWidth })
+    }
+    else {
+      setIndex(0)
+      aboutRef.current.scrollTo({ left: 0 })
+    }
+  }
+  const moveRight = (e) => {
+    e.preventDefault()
+    isSliding = false
+    moveRightLogic()
+  }
+  const moveLeft = (e) => {
+    e.preventDefault()
+    isSliding = false
+    if (index > 0) {
+      setIndex(index - 1)
+      aboutRef.current.scrollBy({ left: -window.innerWidth })
+    }
+    else {
+      setIndex(pages.length - 1)
+      aboutRef.current.scrollTo({ left: window.innerWidth * pages.length })
+    }
+  }
   return (
     <a href="#contact" className="toDownLInk">
-      <section className={aboutStyles.about} id="about">
-        <div className={aboutStyles.aboutText}>
-          <h1 className="title">ՄԵՐ ՄԱՍԻՆ</h1>
-          <p>
-            Հայտնի է, որ ընթերցողը, կարդալով հասկանալի տեքստ, չի կարողանա կենտրոնանալ տեքստի ձևավորման վրա: Lorem Ipsum օգտագործելը բացատրվում է նրանով, որ այն բաշխում է բառերը քիչ
-            թե շատ իրականի նման, ի տարբերություն «Բովանդակություն, բովանդակություն» սովորական կրկննության, ինչը ընթերցողի համար հասկանալի է:
-          </p>
-          <br />
-          <p>
-            Շատ համակարգչային տպագրական ծրագրեր և ինտերնետային էջերի խմբագրիչներ այն օգտագործում են որպես իրենց ստանդարտ տեքստային մոդել, և հետևապես,
-            ինտերնետում Lorem Ipsum-ի որոնման արդյունքում կարելի է հայտնաբերել էջեր, որոնք դեռ նոր են կերտվում:
-          </p>
+      <div className={aboutStyles.aboutContainer} id="about" ref={aboutRef}>
+        <div className="arrows">
+          <div className="arrow" onClick={(e) => { moveLeft(e) }}></div>
+          <div className="arrow arrow2" onClick={(e) => { moveRight(e) }}></div>
         </div>
-        <div className={aboutStyles.aboutImageContainer}>
-          <img className={aboutStyles.companyAbout} src={aboutImage} height="100%" width="100%" />
-        </div>
-      </section>
+        {pages.map((page, i) => {
+          return (
+            <div className={aboutStyles.about} key={i}>
+              <div className={aboutStyles.aboutText}>
+                <h1 className="title">{page.title}</h1>
+                <p>
+                  {page.text}
+                </p>
+                <br />
+                <p>
+                  {page.text2}
+                </p>
+              </div>
+              <div className={aboutStyles.aboutImageContainer}>
+                <img className={aboutStyles.companyAbout} src={page.image} height="100%" width="100%" />
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </a>
   )
 }
--- src/ui/Slider/Slider.jsx
+++ src/ui/Slider/Slider.jsx
@@ -1,10 +1,16 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import sliderStyles from "./Slider.module.css"
 import sliderImage from "../../assets/jpgs/sliderImage.jpg"
 import sliderImage2 from "../../assets/jpgs/sliderImage2.jpg"
 import sliderImage3 from "../../assets/jpgs/sliderImage3.jpg"
 import sliderImage4 from "../../assets/pngs/sliderImage4.png"
 const pages = [
+    {
+        image: sliderImage4,
+        title: "մետաղապլաստե դռների և պատուհանների լայն տեսականի",
+        description: "Էներգախնայող համակարգեր Favorit Space մատչելի գներով",
+        index: 0,
+    },
     {
         image: sliderImage,
         title: "մետաղապլաստե դռների և պատուհանների լայն տեսականի2",
@@ -23,38 +29,69 @@ const pages = [
         description: "Էներգախնայող համակարգեր Favorit Space մատչելի գներով4",
         index: 3,
     },
-    {
-        image: sliderImage4,
-        title: "մետաղապլաստե դռների և պատուհանների լայն տեսականի",
-        description: "Էներգախնայող համակարգեր Favorit Space մատչելի գներով",
-        index: 0,
-    }
 ]
+let isSliding = true
 function Slider() {
     const [index, setIndex] = useState(0)
+    const sliderRef = useRef(null)
     useEffect(() => {
-        setTimeout(() => {
-            if (index + 1 >= pages.length) {
-                setIndex(0)
-            }
-            else {
-                setIndex(index + 1)
-            }
-        }, 1000)
+        if (isSliding) {
+            setTimeout(() => {
+                if (isSliding) {
+                    moveRightLogic()
+                }
+            }, 3000)
+        }
     }, [index])
+    function moveRightLogic() {
+        if (index < pages.length - 1) {
+            setIndex(index + 1)
+            sliderRef.current.scrollBy({ left: window.innerWidth })
+        }
+        else {
+            setIndex(0)
+            sliderRef.current.scrollTo({ left: 0 })
+        }
+    }
+    const moveRight = (e) => {
+        e.preventDefault()
+        isSliding = false
+        moveRightLogic()
+    }
+    const moveLeft = (e) => {
+        e.preventDefault()
+        isSliding = false
+        if (index > 0) {
+            setIndex(index - 1)
+            sliderRef.current.scrollBy({ left: -window.innerWidth })
+        }
+        else {
+            setIndex(pages.length - 1)
+            sliderRef.current.scrollTo({ left: window.innerWidth * pages.length })
+        }
+    }
     return (
         <a href="#advantages" className="toDownLInk">
-            <div className={sliderStyles.slider} style={{backgroundImage: `url(${pages[index].image})`}} id="home">
-            <div className={sliderStyles.sliderContent}>
-                <h1 className={sliderStyles.title}>{pages[index].title}</h1>
-                <p className={sliderStyles.description}>{pages[index].description}</p>
+            <div className={sliderStyles.sliderContainer} id="home" ref={sliderRef}>
+                <div className="arrows">
+                    <div className="arrow" onClick={(e) => { moveLeft(e) }}></div>
+                    <div className="arrow arrow2" onClick={(e) => { moveRight(e) }}></div>
+                </div>
+                {pages.map((page, i) => {
+                    return (
+                        <div className={sliderStyles.slider} style={{ backgroundImage: `url(${page.image})` }} key={i}>
+                            <div className={sliderStyles.sliderContent}>
+                                <h1 className={sliderStyles.title}>{page.title}</h1>
+                                <p className={sliderStyles.description}>{page.description}</p>
+                            </div>
+                        </div>)
+                })}
                 <div className={sliderStyles.pager}>
-                    {pages.map((pageItem,i)=>{
-                        return <div key={i} style={{opacity: i===index ? 0.8 : 0.5}}></div>
+                    {pages.map((pageItem, i) => {
+                        return <div key={i} style={{ opacity: i === index ? 0.8 : 0.5 }}></div>
                     })}
                 </div>
             </div>
-        </div>
         </a>
     )
 }
